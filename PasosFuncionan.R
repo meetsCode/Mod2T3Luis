9eb5846d55ea3345fd9b55a736f9cf03b4f4106a
Luis Sanchez Peña <sanchezlsp@gmail.com>
2017-09-09 15:35:52 +0200

    absorción del trainSet terminada
    En esta versión el código ya absorbe el csv con el trainSet y realiza
    las modificaciones básicas para que los tipos de datos sean los
    apropiados. Aún no he comprobado si las instancias son correctas o si
    hay algún outlier.

--- PasosFuncionan.R
+++ PasosFuncionan.R
@@ -1,20 +1,56 @@
+#!RStudio
+#
+# Pre inicio:
+R.Version()
+cat("\014")    #limpio consola.
+rm(list = ls())   #borro todos los objetos de memoria.
+if(!is.null(dev.list())) dev.off() #pone a cero las ventanas de plot y si no hay ninguna no da error
 
 
-#### APRENDIENDO instalacion ####
+#### 3.1 Import and familiarize yourself with the training set.  ####
 
-install.packages("caret")
-install.packages("mlbench")
-install.packages("caret", dependencies = c("Depends", "Suggests")) 
-# No preguntó sobre instalar los fuentes como en el Mac
-
-install.packages("DiagrammeR") #porque lo necesitó Claudi.
-install.packages("agricolae")  #porque lo necesitó Claudi más adelante.
-
+#### 3.1.1 Librarys ####
+#install.packages("caret")
+#install.packages("caret", dependencies = c("Depends", "Suggests")) 
 library(caret)
-library(mlbench)
 
-data(Sonar)
 
+
+#### 3.1.2 Absorcion datos ####
+dataRaw <- read.csv("/Users/luis/Desktop/Mooc Pharo/dd/Modulo 2/Mod2Task3/Mi trabajo/Mod2T3Luis/Survey_Key_and_Complete_Responses_excel.csv", 
+                    #            header=FALSE, 
+                    stringsAsFactors=FALSE)
+# dataRaw <- read.csv(file.choose())
+str(dataRaw)
+dataFormated <- dataRaw
+
+
+
+#### 3.1.3 Formateo Columnas ####
+dataFormated$salary <- round(dataRaw$salary, digits = 2)
+#dataFormated$age <- factor(dataRaw$age) #Lo hago???
+dataFormated$elevel <- factor(dataRaw$elevel, levels = c(0, 1, 2, 3, 4),
+                              labels = c("Less_than_High_School_Degree", "High_School_Degree", 
+                                         "Some_College", "x4_Year_College_Degree", 
+                                         "Master_s_Doctoral_or_Professional_Degree"))
+
+dataFormated$car <- factor(dataRaw$car, levels = 1:20,
+                           labels = c("BMW", "Buick", "Cadillac", "Chevrolet",
+                                      "Chrysler", "Dodge", "Ford", "Honda", "Hyundai", 
+                                      "Jeep", "Kia", "Lincoln", "Mazda",
+                                      "Mercedes_Benz", "Mitsubishi", "Nissan", "Ram", 
+                                      "Subaru", "Toyota", "None_of_the_above"))
+
+dataFormated$zipcode <- factor(dataRaw$zipcode, levels = 0:8,
+                               labels = c("New_England", "Mid-Atlantic", "East_North_Central", 
+                                          "West_North_Central", "South_Atlantic", "East_South_Central", 
+                                          "West_South_Central", "Mountain", "Pacific"))
+
+dataFormated$credit <- as.integer(dataRaw$credit)
+
+
+dataFormated$brand <- factor(dataRaw$brand, levels = 0:1,
+                             labels = c("Acer", "Sony"))
 
 
 
--- hojaEnSucio.R
+++ hojaEnSucio.R
@@ -17,7 +17,7 @@ dev.list()  #pone a cero las ventanas de plot?
 graphics.off()   #pone a cero las ventanas de plot?
 if(!is.null(dev.list())) dev.off()   #pone a cero las ventanas de plot y si no hay ninguna no da error
 
-
+dataRaw <- read.csv(file.choose())
 #### EJERCICIO. 2.1. Introduction to the caret Package ####
 
 #### 2.1. pruebo uno ya hecho ####
@@ -365,53 +365,37 @@ dataRaw <- read.csv("/Users/luis/Desktop/Mooc Pharo/dd/Modulo 2/Mod2Task3/Mi tra
 str(dataRaw)
 dataFormated <- dataRaw
 
-#dataFormated$age <- factor(dataRaw$age)
 
+dataFormated$salary <- round(dataRaw$salary, digits = 2)
+#dataFormated$age <- factor(dataRaw$age) #Lo hago???
 dataFormated$elevel <- factor(dataRaw$elevel, levels = c(0, 1, 2, 3, 4),
                               labels = c("Less_than_High_School_Degree", "High_School_Degree", 
                                          "Some_College", "x4_Year_College_Degree", 
                                          "Master_s_Doctoral_or_Professional_Degree"))
 
-dataFormated$car <- factor(dataRaw$car, levels = c(0, 1, 2, 3, 4),
-                              labels = c("Less_than_High_School_Degree", "High_School_Degree", 
-                                         "Some_College", "x4_Year_College_Degree", 
-                                         "Master_s_Doctoral_or_Professional_Degree"))
+dataFormated$car <- factor(dataRaw$car, levels = 1:20,
+                              labels = c("BMW", "Buick", "Cadillac", "Chevrolet",
+                                         "Chrysler", "Dodge", "Ford", "Honda", "Hyundai", 
+                                         "Jeep", "Kia", "Lincoln", "Mazda",
+                                         "Mercedes_Benz", "Mitsubishi", "Nissan", "Ram", 
+                                         "Subaru", "Toyota", "None_of_the_above"))
 
-dataFormated$zipcode <- factor(dataRaw$zipcode, levels = c(0, 1, 2, 3, 4),
-                              labels = c("Less_than_High_School_Degree", "High_School_Degree", 
-                                         "Some_College", "x4_Year_College_Degree", 
-                                         "Master_s_Doctoral_or_Professional_Degree"))
+dataFormated$zipcode <- factor(dataRaw$zipcode, levels = 0:8,
+                              labels = c("New_England", "Mid-Atlantic", "East_North_Central", 
+                                         "West_North_Central", "South_Atlantic", "East_South_Central", 
+                                         "West_South_Central", "Mountain", "Pacific"))
 
 dataFormated$credit <- as.integer(dataRaw$credit)
 
 
-dataFormated$brand <- factor(dataRaw$brand, levels = 1:20,
-                               labels = c("BMW", "Buick", "Cadillac", "Chevrolet",
-                                          "Chrysler", "Dodge", "Ford", "Honda", "Hyundai", 
-                                          "Jeep", "Kia", "Lincoln", "Mazda",
-                                          "Mercedes_Benz", "Mitsubishi", "Nissan", "Ram", 
-                                          "Subaru", "Toyota", "None_of_the_above"))
-
-5) What is your zip code?	
-Respondents enter zip code, which is captured as 1 of the following 9 regions in the U.S.	
-Value	Region
-0	New England
-1	Mid-Atlantic
-2	East North Central
-3	West North Central
-4	South Atlantic
-5	East South Central
-6	West South Central
-7	Mountain
-8	Pacific
+dataFormated$brand <- factor(dataRaw$brand, levels = 0:1,
+                               labels = c("Acer", "Sony"))
+	
 
 
-7) Which brand of computers do you prefer?	
-Respondents select from the following 2 choices:	
-  Value 	Description
-0	Acer
-1	Sony
 
+
+source("absorcionYPreparacionDatos.R", echo = TRUE)
 
 
 #### 3.2 Using createDataPartition create training and testing sets.  ####
